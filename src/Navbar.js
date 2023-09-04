c1b7f0ca015211a16a9a0a8bd92ec301fdd3184d
Paramhans Singh <paramhanssingh084@gmail.com>
2023-09-04 14:22:35 +0530

    Added FilmSafari - Movie App (React Js)

---
+++ src/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from './nav.module.css'
+import cart from "./assets/shopping-cart.png"
+
+function Navbar(props) {
+  
+    const {cartCount} =  props;
+       
+      return (
+      <>
+      <div className={styles.nav}>
+        
+      <div className={styles.title}>MOVIE APP</div>
+        
+        <div className={styles.cartIconContainer}>
+            <img className={styles.cartIcon} 
+                 alt="Cart-Icon" 
+                 src = {cart}
+                 />
+            <span className ={styles.cartCount}>{cartCount}</span>
+        </div>
+      </div>
+
+      </>
+      )
+    }
+  
+ 
+  
+  export default Navbar;
+  
---
+++ src/moviesData.js
@@ -0,0 +1,74 @@
+export const movies = [{
+    title: "Pathaan",
+    plot: "Pathaan is a RAW agent, who along with his senior officer Major Nandini Grewal open a unit dubbed Joint Operations and Covert Research in order to recruit former RAW agents and soldiers who were forced to retire due to past trauma or injury, but want to continue serving the country.",
+    price: 350,
+    rating: 9.0,
+    poster: "https://upload.wikimedia.org/wikipedia/en/c/c3/Pathaan_film_poster.jpg",
+    stars: 0,
+    fav: false,
+    isInCart: false
+},
+
+{
+    title: "Gadar 2",
+    plot: "Following the events in the previous film, a Pakistani Army General, Hamid Iqbal, gets Ashraf Ali executed for aiding Tara Singh and his family in their escape from Pakistan to India. Iqbal wants to exact vengeance on Tara for eliminating 40 soldiers during his escape.",
+    price: 250,
+    rating: 8.9,
+    poster: "https://upload.wikimedia.org/wikipedia/en/6/62/Gadar_2_film_poster.jpg",
+    stars: 0,
+    fav: false,
+    isInCart: false
+},
+{
+    title: "OMG 2",
+    plot: "OMG 2 is the story of Kanti Sharan Mudgal, a staunch devotee of Lord Shiva; a simple man, a loving father and a caring husband. One day his son, is blamed for immoral conduct (coming out as gay) and is thrown out of school. ",
+    price: 280,
+    rating: 8.0,
+    poster: "https://upload.wikimedia.org/wikipedia/en/5/56/OMG_2_%E2%80%93_Oh_My_God%21_2_poster.jpg",
+     stars: 0,
+    fav: false,
+    isInCart: false
+},
+
+{
+    title: "Tu Jhoothi Main Makkaar",
+    plot: "Rohan Arora is the son of Ramesh Arora, a rich Punjabi business magnate, and lives with his family in Delhi. He and his best friend Manu Dabbas secretly work as break-up consultants, helping people break-up with their respective partners. ",
+    price: 180,
+    rating: 8.2,
+    poster: "https://upload.wikimedia.org/wikipedia/en/b/b6/Tu_Jhoothi_Main_Makkaar_Title_Card.jpeg",
+     stars: 0,
+    fav: false,
+    isInCart: false
+},
+
+{
+    title: "Adipurush",
+    plot: "Thousands of years ago, the rakshasa king of Lanka, Lankesh, obtains a boon from Brahma, the god of creation, with which he cannot not be slain by the gods or demons, during day or night, thus making him invincible. Elsewhere, many years afterwards in the kingdom of Ayodhya, King Dashratha of Kosala, exiles his son, Raghava for 14 years to fulfill his promise to his wife, Kaikeyi, Raghava's stepmother, that their son, Bharat, would ascend the throne.",
+    price: 250,
+    rating: 8.2,
+    poster: "https://upload.wikimedia.org/wikipedia/en/c/cf/Adipurush_poster.jpeg",
+     stars: 0,
+    fav: false,
+    isInCart: false
+},
+{
+    title: "Rocky Aur Rani Ki Prem Kahani",
+    plot: "The Randhawas are a Punjabi business family who own the prosperous Dhanlakshmi Sweets, named after their matriarch Dhanlakshmi Randhawa. Her husband, Kanwal Lund, is amnesiac and wheelchair bound after an accident soon after his marriage.",
+    price: 260,
+    rating: 8.0,
+    poster: "https://upload.wikimedia.org/wikipedia/en/6/65/Rocky_Aur_Rani_Ki_Prem_Kahani.jpg",
+     stars: 0,
+    fav: false,
+    isInCart: false
+},
+{
+    title: "Satyaprem Ki Katha",
+    plot: "Satyaprem alias Sattu is a happy-go-lucky young man who lives with his middle-class Gujarati family in Ahmedabad. He has failed his law degree and dreams of getting married to a beautiful girl but always gets rejected for being jobless. ",
+    price: 180,
+    rating: 8.6,
+    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/90/Satyaprem_Ki_Katha_poster.jpg/220px-Satyaprem_Ki_Katha_poster.jpg",
+     stars: 0,
+    fav: false,
+    isInCart: false
+},
+]
